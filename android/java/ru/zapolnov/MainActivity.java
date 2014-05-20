050b5852c47e7ede9ceed9f6ba704e9e140e583d
Nikolay Zapolnov <zapolnov@gmail.com>
2014-05-20 22:53:58 +0400

    Preliminary support for Android.

--- android/java/ru/zapolnov/MainActivity.java
+++ android/java/ru/zapolnov/MainActivity.java
@@ -21,15 +21,25 @@ public final class MainActivity extends Activity
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 			WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+		int[] opts = new int[6];
+		nativeGetInitOptions(opts);
+		int redBits = opts[0];
+		int greenBits = opts[1];
+		int blueBits = opts[2];
+		int alphaBits = opts[3];
+		int depthBits = opts[4];
+		int stencilBits = opts[5];
+
 		m_GLView = new GLSurfaceView(this);
 		m_GLView.setEGLContextClientVersion(2);
-		m_GLView.setEGLConfigChooser(8, 8, 8, 8, 24, 8);
-//		m_GLView.setPreserveEGLContextOnPause(true);
+		m_GLView.setEGLConfigChooser(redBits, greenBits, blueBits, alphaBits, depthBits, stencilBits);
 
 		m_GLView.setRenderer(new GLSurfaceView.Renderer() {
+			private long m_PrevTime;
 			private int m_Width;
 			private int m_Height;
 			@Override public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+				m_PrevTime = System.currentTimeMillis();
 				nativeInit();
 			}
 			@Override public final void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -37,7 +47,10 @@ public final class MainActivity extends Activity
 				m_Height = height;
 			}
 			@Override public final void onDrawFrame(GL10 gl) {
-				nativeRunFrame(m_Width, m_Height);
+				long newTime = System.currentTimeMillis();
+				long timeDelta = newTime - m_PrevTime;
+				m_PrevTime = newTime;
+				nativeRunFrame(m_Width, m_Height, timeDelta);
 			}
 		});
 
@@ -56,8 +69,9 @@ public final class MainActivity extends Activity
 		m_GLView.onResume();
 	}
 
+	public static native void nativeGetInitOptions(int[] opts);
 	public static native void nativeInit();
-	public static native void nativeRunFrame(int width, int height);
+	public static native void nativeRunFrame(int width, int height, long time);
 
 	static {
 		System.loadLibrary("code");
